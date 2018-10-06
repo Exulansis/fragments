92ff3810dffc70f3a019549fa4fba9ffd88dc739
Exulansis <rusu.eugeniu@gmx.de>
2018-10-06 14:50:58 +0200

    Added comments, updated dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import { getUserInput } from "./utils"
 
+/*
+ * User asked for a random digit 10 times
+ *
+ * Evaluate how random the input is
+ * (o) Compare against a random number generator
+ * Print out the results
+*/
+
 async function runProgram () {
-  // Code goes here
+  /*
+   * Function to receive user input
+   * const userInput = await getUserInput('Please provide your age')
+  */
 }
 
 runProgram()
